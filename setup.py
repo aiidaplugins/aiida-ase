a1b0223a3d2e2655c109e29f4bcf787bc96b3553
Sebastiaan Huber <mail@sphuber.net>
2021-06-24 12:51:02 +0200

    Package: move package description to `setup.cfg` and update it
    The `setup.py` can be simplified to a simple shim, which is required to
    still allow editable installs. For all normal installs, the `setup.cfg`,
    which replaces the `setup.json`, is sufficient to make the package
    installable, together with the `pyproject.toml` that defines the build
    requirements.

--- setup.py
+++ setup.py
@@ -1,34 +1,7 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Define the setup for the `aiida-ase` plugin."""
-try:
-    import fastentrypoints  # pylint: disable=unused-import
-except ImportError:
-    # This should only occur when building the package, i.e. for `python setup.py sdist/bdist_wheel`
-    pass
-
-
-def setup_package():
-    """Install the `aiida-ase` package."""
-    import json
-    from setuptools import setup, find_packages
-
-    filename_setup_json = 'setup.json'
-    filename_description = 'README.md'
-
-    with open(filename_setup_json, 'r') as handle:
-        setup_json = json.load(handle)
-
-    with open(filename_description, 'r') as handle:
-        description = handle.read()
-
-    setup(
-        include_package_data=True,
-        packages=find_packages(),
-        long_description=description,
-        long_description_content_type='text/markdown',
-        **setup_json
-    )
-
+"""Shim setup file that allows editable installs."""
+import setuptools
 
 if __name__ == '__main__':
-    setup_package()
+    setuptools.setup()
