92c73c2ce232d19b3d18285fd20b2e0b80d29ad3
Sudarshan Vijay <47235474+sudarshanv01@users.noreply.github.com>
2021-08-10 10:42:45 +0200

    `AseCalculation`: add `CMDLINE` key to `settings` node (#12)
    This setting allows to define additional command line parameters, such
    as adding the `python` executable, which is necessary to run the GPAW
    executable. This is explained in the updated `README.md` and a new
    example script is added, removing the outdated existing ones.
    
    Finally, a new option is added `optimizer_stdout` which takes the filename
    of the file to which the output during a relaxation will be written.

---
+++ examples/example_pw_gpaw_scf.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+from aiida.orm import load_code
+from ase.build import bulk
+from aiida import orm, engine
+
+def runner():
+
+    AseCalculation = CalculationFactory('ase.ase')
+    builder = AseCalculation.get_builder()
+
+    code = load_code('gpaw-21.6.0@localhost')
+    builder.code = code
+
+    # generate an example structure
+    atoms = bulk('Si', 'diamond', a=5.4)
+    StructureData = DataFactory('structure')
+    structure = StructureData(ase=atoms)
+    builder.structure = structure
+
+    # k-point information
+    KpointsData = DataFactory('array.kpoints')
+    kpoints = KpointsData()
+    kpoints.set_kpoints_mesh([1,1,1])
+    builder.kpoints = kpoints
+
+    parameters = {
+    'calculator': {
+        'name': 'gpaw',
+        'args': {
+            'mode': {
+                '@function': 'PW',
+                'args': {'ecut': 300}},
+            'convergence': {'energy': 1e-9},
+            'occupations': {
+                'name': 'fermi-dirac',
+                'width':0.05}
+            },
+        },
+    }
+
+    builder.parameters = orm.Dict(dict=parameters)
+
+    # Running the calculation using gpaw python
+    settings = {'CMDLINE': ['python']}
+    builder.settings = orm.Dict(dict=settings)
+
+    builder.metadata.options.resources = {'num_machines': 1}
+    builder.metadata.options.max_wallclock_seconds = 1 * 30 * 60
+    builder.metadata.options.withmpi = False
+
+
+    engine.submit(builder)
+
+
+if __name__ == '__main__':
+    runner()
