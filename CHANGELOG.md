f96871df07b6aa1eacc694ec50edeab5024db392
Sebastiaan Huber <mail@sphuber.net>
2023-03-17 16:27:56 +0100

    Release `v2.0.0`

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Change log
+
+## `v2.0.0` - 2023-03-17
+
+This version is compatible with `aiida-core~=1.0`.
--- src/aiida_ase/__init__.py
+++ src/aiida_ase/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 """The official AiiDA plugin for ASE."""
-__version__ = '2.0.0a1'
+__version__ = '2.0.0'
