aa833639679b4874a09d4a5f8a61a84bf6dae53a
Keilbart <keilbart1@fireman.llnl.gov>
2024-08-29 09:31:11 -0600

    Created an example file for running INQ calculation.

---
+++ examples/calculations/example_inq.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""Example script of how to perform an SCF GPAW calculation on crystalline silicon using AiiDA."""
+from aiida import engine, orm
+from aiida.orm import load_code
+from aiida.plugins import DataFactory, CalculationFactory
+from ase.build import bulk
+
+# Change the following value to the ``Code`` that you have configured
+CODE_NAME = 'pinq@localhost'
+
+Dict = DataFactory('core.dict')
+StructureData = DataFactory('core.structure')
+KpointsData = DataFactory('core.array.kpoints')
+AseCalculation = CalculationFactory('ase.ase')
+
+
+def main():
+    # generate an example structure
+    atoms = bulk('Si', 'diamond', a=5.4)
+    structure = StructureData(ase=atoms)
+
+    # k-point information
+    kpoints = KpointsData()
+    kpoints.set_kpoints_mesh([1,1,1])
+
+    parameters = {
+        'calculator': {
+            'name': 'inq',
+            'args': {
+                'ecut': 300,
+                'convergence': {'energy': 1e-9},
+                'occupations': {
+                    'name': 'fermi-dirac',
+                    'width': 0.05
+                }
+            }
+        }
+    }
+
+    # Running the calculation using GPAW Python
+    settings = {}
+
+    builder = AseCalculation.get_builder()
+    builder.code = load_code(CODE_NAME)
+    builder.structure = structure
+    builder.kpoints = kpoints
+    builder.parameters = orm.Dict(parameters)
+    builder.settings = orm.Dict(settings)
+    builder.metadata.options.resources = {'num_machines': 1}
+    builder.metadata.options.max_wallclock_seconds = 30 * 60  # 30 minutes
+    builder.metadata.options.withmpi = True
+
+    node = engine.submit(builder)
+    print(f'AseCalculation<{node.pk}> submitted to the daemon.')
+
+
+if __name__ == '__main__':
+    main()
